0cd7e920d794989b9c5c7aa14f8b8a9244d32580
Alex Prather <prathera14@gmail.com>
2020-03-02 18:56:29 +0800

    finished populated the index page with the seeded data

--- app/models/spaceship.rb
+++ app/models/spaceship.rb
@@ -1,4 +1,5 @@
 class Spaceship < ApplicationRecord
+  validates :name, :passengers, :length, :speed, :spaceship_class, :crew, :location, :manufacturer, :description, presence: true
   has_many :reviews
   belongs_to :user
 end
