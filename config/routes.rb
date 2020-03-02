2fffae569b55aa01806ac2ed420f749aaa369256
Will Price <pricew86@gmail.com>
2020-03-02 16:18:20 +0800

    finished routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #SPACESHIPS
+  resources :spaceships do
+    resources :bookings, only: [:new, :create]
+    resources :reviews, only: [:new, :create]
+  end
+  
+  resources :bookings, only: [:show]
+  # get '/spaceships', to: 'spaceships#index', as: :spaceships
+  # get '/spaceships/:id', to: 'spaceships#show', as: :spaceship
+  # get '/spaceships/:id/new', to: 'spaceships#new', as: :new_spaceship
+  # post '/spaceships', to: 'spaceships#create'
+  # get 'spaceships/:id/edit', to:
+  get '/dashboard', to: 'pages#dashboard', as: :dashboard
 end
