78e8f1e69dc7fe218c30222e9abe2c00dde184ce
Will Price <32665615+flip-in@users.noreply.github.com>
2020-03-03 12:16:09 +0800

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,4 @@
 # Load the Rails application.
 require_relative 'application'
-# CLOUDINARY_URL=cloudinary://686846358249167:Grgbiw-e3MNVsDKNDbtwLLOnAAY@dom5zyzgt
 # Initialize the Rails application.
 Rails.application.initialize!
