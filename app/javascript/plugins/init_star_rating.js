56f210c6e8b414131a52425bdc4695ceb4faa07a
Will Price <pricew86@gmail.com>
2020-03-03 15:58:56 +0800

    added a jquery form for review stars

---
+++ app/javascript/plugins/init_star_rating.js
@@ -0,0 +1,10 @@
+import "jquery-bar-rating";
+import "jquery-bar-rating/dist/themes/css-stars.css";
+// import $ from 'jquery';
+
+const initStarRating = () => {
+  $('#review_stars').barrating({
+    theme: 'css-stars'
+  });
+}
+export { initStarRating };
