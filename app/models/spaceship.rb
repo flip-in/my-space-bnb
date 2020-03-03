b270ac86addcadc98e890e85d5cb8cb67db1f33e
Will Price <pricew86@gmail.com>
2020-03-03 10:58:05 +0800

    some fixes

--- app/models/spaceship.rb
+++ app/models/spaceship.rb
@@ -1,5 +1,6 @@
 class Spaceship < ApplicationRecord
   validates :name, :passengers, :length, :speed, :spaceship_class, :crew, :location, :manufacturer, :description, presence: true
-  has_many :reviews
+  has_many :bookings
+  has_many :reviews, through: :bookings
   belongs_to :user
 end
