ef74b3b379cd37d096a9a1fbfc9e7758107703c5
Alex Prather <prathera14@gmail.com>
2020-03-03 18:04:08 +0800

    we added the bookings and separated them based on current and past so that you can update a booking or add a review to a past booking

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,8 @@ class PagesController < ApplicationController
   end
 
   def dashboard
+    @current_bookings = current_user.bookings.select { |booking| booking.end_date >= Date.today }
+    @past_bookings = current_user.bookings.select { |booking| booking.end_date < Date.today }
+
   end
 end
