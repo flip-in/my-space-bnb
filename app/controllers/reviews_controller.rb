6e6c7c617334f8bfc86e828b6448677fb3430a4e
Will Price <pricew86@gmail.com>
2020-03-03 17:41:26 +0800

    added a sweet ass form for the reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,17 +9,18 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @review.user = current_user
     @review.spaceship = @spaceship
-    
     if @review.save
       redirect_to spaceship_path(@spaceship)
     else
-      flash[:alert] = "Something went wrong."
+      flash[:alert] = 'Something went wrong.'
       render :new
     end
   end
+
   private
-  
+
   def review_params
     params.require(:review).permit(:content, :stars)
   end
+
 end
