b2aff31cc2ed516e49575440757aa5bd1778948b
Will Price <pricew86@gmail.com>
2020-03-05 13:55:36 +0800

    added bunch of attributes to user, can upload a photo in their edit profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :reviews
   has_many :bookings
   has_many :spaceships
+  has_one_attached :photo
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
