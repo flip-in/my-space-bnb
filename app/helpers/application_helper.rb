414a10e27f7d8b6bec0f83aa2b841f4ed8ff6bb8
Will Price <pricew86@gmail.com>
2020-03-06 12:12:22 +0800

    helper function for avatars

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,10 @@
 module ApplicationHelper
-  def avatar_for_navbar(user)
+  def avatar_for(user, data = {})
     @avatar = user.avatar
     if @avatar.attached?
-      @avatar_user = cl_image_tag(@avatar.key, height: 40, width: 40)
+      @avatar_user = cl_image_tag(@avatar.key, data)
     else
-      @avatar_user = image_tag("yoda.png", height: 40, width: 40)
-    end
-    return @avatar_user
-  end
-
-  def avatar_for_background(user)
-    @avatar = user.avatar
-    if @avatar.attached?
-      @avatar_user = cl_image_path(@avatar.key)
-    else
-      @avatar_user = image_path("yoda.png")
+      @avatar_user = image_tag("yoda.png", data)
     end
     return @avatar_user
   end
