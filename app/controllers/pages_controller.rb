835c5d0c727986a20e22be427060936b2eb806b9
Will Price <pricew86@gmail.com>
2020-03-06 18:54:45 +0800

    fixed authentications

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, raise: false
+  skip_before_action :authenticate_user!, only: [:home]
   def home
     @spaceships = Spaceship.all
     @spaceships = @spaceships.sample(3)
@@ -8,6 +8,5 @@ class PagesController < ApplicationController
   def dashboard
     @current_bookings = current_user.bookings.select { |booking| booking.end_date >= Date.today && (booking.status == 'pending' || booking.status == 'confirmed') }
     @past_bookings = current_user.bookings.select { |booking| booking.end_date < Date.today && (booking.status == 'pending' || booking.status == 'confirmed') }
-
   end
 end
